3683f8975fd92ba4dc38ec9ed4fd2380e068c07b
wuweijie <wu.weijie@outlook.com>
2018-04-09 15:23:44 +0800

    修改一部分的

--- src/main/java/com/keyiu/java/controller/UserRestController.java
+++ src/main/java/com/keyiu/java/controller/UserRestController.java
@@ -1,12 +1,17 @@
 package com.keyiu.java.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserRestController {
+    @Value("${spring.datasource.username}")
+    private String username;
+
     @RequestMapping("/")
     public String findAllUsers() {
+        System.out.println(this.username);
         return "all users";
     }
 }
